2b6dcc99d5f9393e0ec0494f6d1069b791bfa294
Sree Vardhan Reddy <sreevardhan1729@gmail.com>
2024-09-30 09:10:43 +0530

    part-1d3

--- part1_course/src/App.jsx
+++ part1_course/src/App.jsx
@@ -1,23 +1,28 @@
 import { useState } from "react"
-const Button = ({onClick,text}) => {
-  return(
-    <>
-      <button onClick={onClick}>{text}</button>
-    </>
-  )
-}
-const App = (props) => {
-  const [counter,setCounter] = useState(0)
-  const add = () => setCounter(counter+1)
-  const reset = () => setCounter(0)
-  const remove = () => setCounter(counter-1);
+const App = () => {
+  const [left,setLeft] = useState(0)
+  const [right,setRight] = useState(0)
+  const [AllClicks,setAll] = useState([])  
+  const [total,Settotal] = useState(0)
+  const handleLeft = () => {
+    setAll(AllClicks.concat('L'))
+    setLeft(left+1)
+    Settotal(total+1)
+  }
+  const handleRight = () =>{
+    setAll(AllClicks.concat('R'))
+    setRight(right+1)
+    Settotal(total+1)
+  }
   return (
-    <>
-      <div>{counter}</div>
-      <Button onClick={add} text = "plus"/>
-      <Button onClick={reset} text = "reset"/>
-      <Button onClick={remove} text = "remove"/>
-    </>
+    <div>
+      {left}
+      <button onClick={handleLeft}>left</button>
+      <button onClick={handleRight}>right</button>
+      {right}
+      <p>{AllClicks.join(' ')}</p>
+      <p>{total}</p>
+    </div>
   )
 }
 export default App
